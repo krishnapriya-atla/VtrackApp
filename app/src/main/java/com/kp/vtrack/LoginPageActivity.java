b899861a3fe02c61240a75e726699732df44b923
krishnapriya-atla <priya.18mis7061@vitap.ac.in>
2022-07-07 17:02:19 +0530

    login page

---
+++ app/src/main/java/com/kp/vtrack/LoginPageActivity.java
@@ -0,0 +1,30 @@
+package com.kp.vtrack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class LoginPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+        ImageView img = (ImageView)findViewById(R.id.profile_image);
+
+        img.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://vtrack3.netlify.app/"));
+                startActivity(intent);
+            }
+        });
+    }
+}
